ee351ca804982f02f35b6019b1e33630826325c3
Bryan Phelps <bryphe@outlook.com>
2018-10-16 11:34:04 -0700

    Update appveyor, add create-release script

---
+++ esy/create-release.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require("path");
+
+const releaseFolder = process.env["FLEXDLL_RELEASE_FOLDER"];
+
+if (!releaseFolder) {
+    console.error("[FlexDLL Packager]: FLEXDLL_RELEASE_FOLDER environment variable must be set");
+} else {
+    console.log(`[FlexDLL Packager]: Using release folder ${releaseFolder}.`);
+}
+
+const filesToBundle = [
+    "flexlink.exe",
+    "flexdll_initer_mingw64.o",
+    "flexdll_mingw64.o",
+    "esy/package.json",
+];
+
+if (!fs.existsSync(releaseFolder)) {
+    console.log(`[FlexDLL Packager]: Creating release folder ${releaseFolder}.`);
+    fs.mkdirSync(releaseFolder);
+}
+
+filesToBundle.forEach((f) => {
+    fs.copySync(path.join(__dirname, "..", f), path.join(releaseFolder, path.basename(f)));
+});
